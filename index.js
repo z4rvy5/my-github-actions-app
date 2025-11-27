97c1f1caa9583aa22b394c133d2ddebc3f400740
z4rvy5 <arvys045@gmail.com>
2025-11-27 12:54:14 +0530

    Trigger deployment using Github Actions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(' App deployed using GitHub Actions + Docker + AWS EC2');
+  res.send(' Auto deployed using GitHub Actions + Docker + AWS EC2');
 });
 
 app.listen(3000, () => {
