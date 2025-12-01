09d1218bea1342be96c39156d8783ff8cafe1cb1
z4rvy5 <arvys045@gmail.com>
2025-12-01 12:16:03 +0530

    Fix Jenkins auto deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(' Content auto deployed using Jenkins Webhook.');
+  res.send(' Auto deployed using Jenkins Webhook.');
 });
 
 app.listen(3000, () => {
