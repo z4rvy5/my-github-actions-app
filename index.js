a1649c217bdcaa9d404a7dd76f13aaf8e9b13588
z4rvy5 <arvys045@gmail.com>
2025-12-01 12:34:41 +0530

    Final Jenkins auto deploy test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(' Auto deployed using Jenkins Webhook.');
+  res.send(' Full auto deployment using Jenkins.');
 });
 
 app.listen(3000, () => {
