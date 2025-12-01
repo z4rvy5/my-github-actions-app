d60ee9fc0e8ebbd9268e6cea3c52b9979cd9c55b
z4rvy5 <arvys045@gmail.com>
2025-12-01 12:40:37 +0530

    Final pipeline fix test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(' Full auto deployment using Jenkins.');
+  res.send(' Full auto deployment using Jenkins - Working Test.');
 });
 
 app.listen(3000, () => {
